c8f28a4d45d8306e5476b7c190ed82db2efd6c83
Fredrik93 <fredrikullman93@gmail.com>
2025-11-07 06:00:45 +0100

    wip

--- leetcode/sum_of_xor_subsets.py
+++ leetcode/sum_of_xor_subsets.py
@@ -1,5 +1,12 @@
 from typing import List
 
 def Total (nums: List[int]) -> int:
-    print("test")
+    # loop over nums and find all subsets 
+    for i in range(len(nums)):
+    # save each subset and check its xor total 
+        for j in range(len(nums)):
+            print("subset: ", nums[i:j])
+    # save total to an int totalNum
+    # return totalNum
+
     return -1
--- test_sum_of_xor_subsets.py
+++ test_sum_of_xor_subsets.py
@@ -1,12 +1,17 @@
 import unittest
 import leetcode.sum_of_xor_subsets as subsets
 
-class TestTwoSums(unittest.TestCase):
+class TestXORTotal(unittest.TestCase):
 
     def test_1(self):
         
         result = subsets.Total([1,3])
         self.assertEqual(result, 6)
+
+    def test_2(self):
+        
+        result = subsets.Total([5,1,6])
+        self.assertEqual(result, 28)
 
 if __name__ == "__main__":
         unittest.main()
