0c9bcf7eae3a6f7f6ee236f289945e6637fd109e
Fredrik93 <fredrikullman93@gmail.com>
2025-11-10 06:33:03 +0100

    wip

--- leetcode/sum_of_xor_subsets.py
+++ leetcode/sum_of_xor_subsets.py
@@ -1,11 +1,16 @@
 from typing import List
 
 def Total (nums: List[int]) -> int:
+    s = set()
+    list_of_sets = []
     # loop over nums and find all subsets 
     for i in range(len(nums)):
-    # save each subset and check its xor total 
+    # save each subset and check its xor total                 
         for j in range(len(nums)):
-            print("subset: ", nums[i:j])
+           tmp = nums[i:j]
+           # Dont add empty sets to the list
+           if tmp:
+            list_of_sets.append(tmp)
     # save total to an int totalNum
     # return totalNum
 
