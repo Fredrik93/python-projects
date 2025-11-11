b9e2927459f1b861978e8e9b122ed28ae2e36646
Fredrik93 <fredrikullman93@gmail.com>
2025-11-11 07:07:21 +0100

    wip

--- leetcode/sum_of_xor_subsets.py
+++ leetcode/sum_of_xor_subsets.py
@@ -1,7 +1,7 @@
 from typing import List
 
 def Total (nums: List[int]) -> int:
-
+    totalXOR = 0
     list_of_sets = []
     for i in range(len(nums)):
         for j in range(len(nums)+1):
@@ -9,7 +9,18 @@ def Total (nums: List[int]) -> int:
            # Dont add empty sets to the list
            if tmp:
             list_of_sets.append(tmp)
-    # save total to an int totalNum
-    # return totalNum
+    
+    # iterate over each set and perform the xor operation
+    for set in list_of_sets:
+        print("set: " , set)
+        if len(set) == 1:
+           totalXOR = totalXOR + set[0]
+        else:
+            tmpXor = 0
+            for i in range(len(set)-1):
+               tmpXor = set[i] ^ set[i+1]
+               totalXOR = totalXOR + tmpXor
 
-    return -1
+
+    return totalXOR
+
