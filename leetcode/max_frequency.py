ba4a65161acdb234130cacf26b4ca4db8d6b07f8
Fredrik93 <fredrikullman93@gmail.com>
2025-11-17 05:36:02 +0100

    wip

--- leetcode/max_frequency.py
+++ leetcode/max_frequency.py
@@ -1,4 +1,9 @@
 class MaxyMax:
     def maxFreqSum(self, s: str) -> int:
+        # save currentChar 
+        # compare to all other chars and count occurence 
+        # if its a vowel, check if its the most frequently used and add to variable
+
+        # if its a consonant, check if its the most frequently used and add to variable
         return 0
         
--- test_max_frequency.py
+++ test_max_frequency.py
@@ -6,7 +6,7 @@ class TestMaxFreqSum(unittest.TestCase):
     def test_returns_zero(self):
         obj = leetcode.max_frequency.MaxyMax()
         result = obj.maxFreqSum("successes")
-        self.assertEqual(result, 0)
+        self.assertEqual(result, 6)
 
 
 if __name__ == "__main__":
