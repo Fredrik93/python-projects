52c6ed0d355d2a5fcbe1a83f3e71dadb636d1aa6
Fredrik93 <fredrikullman93@gmail.com>
2026-01-11 07:45:17 +0100

    wip

--- leetcode/reverse_prefix_string.py
+++ leetcode/reverse_prefix_string.py
@@ -1,5 +1,11 @@
 class RevPref:
 
     def reversePrefix(self, s:str, k: int) -> str:
-
+        # get the kth substring of the string 
+        subString = s[0:k]
+        restOfTheString = s[k:]
+        # get the rest of the string (kth - string length)
+        # reverse the substring
+        # concaternate the end of the string to the reversed substring
+        # return value 
         return "hi"
