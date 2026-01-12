5c6aec71b1770b6c0c6fd7c222aeb5f7ccb82073
Fredrik93 <fredrikullman93@gmail.com>
2026-01-12 05:13:49 +0100

    wip

--- leetcode/goal_parser.py
+++ leetcode/goal_parser.py
@@ -1,17 +1,18 @@
 class GoalParser:
     def interpret(self, command: str) -> str:
         # split upp command, save to list 
-        
+        command = ["G","G"]
         
         # switch for each cmd 
         result = ""
-        match command:
-            case "G":
-                result += "G"
-            case "()":
-                result += "o"
-            case "(al)":
-                result += "al"
-        # concqternqte the result and return 
+        
+        for cmd in command:
+            match cmd:
+                case "G":
+                    result += "G"
+                case "()":
+                    result += "o"
+                case "(al)":
+                    result += "al"
 
         return result
