af30c979802d6c3a919855847954f3e5b542b405
Fredrik93 <fredrikullman93@gmail.com>
2026-01-14 05:28:33 +0100

    test passes

--- leetcode/target_emp_hours.py
+++ leetcode/target_emp_hours.py
@@ -3,6 +3,12 @@ from typing import List
 
 class EmpTarget:
     def numberOfEmployeesWhoMetTarget(self, hours:List[int], target:int) -> str:
-        res = target + 2
-        return res
+        amountOfPeopleHittingTarget = 0
+        # loop over list.
+        for hour in hours:
+            if hour >= target:
+                amountOfPeopleHittingTarget += 1
+        # if el == target then increment counter
+        # return counter
+        return amountOfPeopleHittingTarget
     
--- tests/test_target_emp_hours.py
+++ tests/test_target_emp_hours.py
@@ -2,5 +2,5 @@ from leetcode.target_emp_hours import EmpTarget
 
 def test_basic_case():
     sol = EmpTarget()
-    assert sol.numberOfEmployeesWhoMetTarget([1,2,3],2) == 4
+    assert sol.numberOfEmployeesWhoMetTarget([1,2,3],2) == 2
 
