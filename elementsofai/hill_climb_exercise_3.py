2d15116ac98b2f418e7a814be859351829b7c9f7
Fredrik93 <fredrikullman93@gmail.com>
2026-01-15 06:18:49 +0100

    ex 3 done

--- elementsofai/hill_climb_exercise_3.py
+++ elementsofai/hill_climb_exercise_3.py
@@ -2,46 +2,33 @@ import math
 import random             	# just for generating random mountains                                 	 
 
 # generate random mountains                                                                               	 
-
-w = [.05, random.random()/3, random.random()/3]
-h = [1.+math.sin(1+x/.6)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
+w = [random.random()/3, random.random()/3, random.random()/3]
+h = [1.+math.sin(1+x/6.)*w[0]+math.sin(-.3+x/9.)*w[1]+math.sin(-.2+x/30.)*w[2] for x in range(100)]
+h[0] = 0.0; h[99] = 0.0
 
 def climb(x, h):
-    leftSteps = 5;
-    rightSteps = 5;
-
     # keep climbing until we've found a summit
     summit = False
 
+    
     # edit here
     while not summit:
-        print("entered")
-        summit = True         # stop unless there's a way up
-        if h[x + 1] > h[x]:
-            x = x + 1
-            rihtSteps = rightSteps-1         # right is higher, go there
-            summit = False
-            print("rightSteps count: ", rightSteps)    # and keep going
-        elif h[x-1] > h[x]:
-            x = x - 1
-            summit = False
-            leftSteps = leftSteps-1
-            print("leftsteps count: ", leftSteps)
-    
-    if leftSteps == 0: 
-        print("entered leftSteps 0: ", x)
-        return x
-    if rightSteps == 0:
-        print("entered rightSteps 0: ", x)
-        return x
-    return -1
+        summit = True
+        for x_new in range(max(0,x-5), min(99,x+5)):
+            if h[x_new] > h[x]:
+                x = x_new
+                summit = False
+            
+    return x
 
 
 def main(h):
+
     # start at a random place                                                                                  	 
     x0 = random.randint(1, 98)
     x = climb(x0, h)
 
+    print("Venla started at %d and got to %d" % (x0, x))
     return x0, x
 
 main(h)
---
+++ tests/test_smoke.py
@@ -0,0 +1,3 @@
+def test_smoke():
+    assert True
+# used for checking that test environment works. this should be able to run using the little play button to the left of the test name test_smoke()
