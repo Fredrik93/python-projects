8f43835d717932d97b474e177173179a5a936793
Fredrik93 <fredrikullman93@gmail.com>
2026-01-22 06:11:02 +0100

    wip

--- elementsofai/fishing_in_nordics_ex8.py
+++ elementsofai/fishing_in_nordics_ex8.py
@@ -1,18 +1,31 @@
-def main():
-    countries = ['Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden']
-    populations = [5615000, 5439000, 324000, 5080000, 9609000]
-    fishers = [1891, 2652, 3800, 11611, 1757]
-    probabilities = []
-    total_fishers = sum(fishers)
-    total_population = sum(populations)
+countries = ['Denmark', 'Finland', 'Iceland', 'Norway', 'Sweden']
+populations = [5615000, 5439000, 324000, 5080000, 9609000]
+male_fishers = [1822, 2575, 3400, 11291, 1731]
+female_fishers = [69, 77, 400, 320, 26] 
+total_fishers = sum(male_fishers) + sum(female_fishers)
+female_winners = []
 
-    for i, countryPop in enumerate(populations):
-        res = (fishers[i] / total_fishers) * 100
-        probabilities.append(res)
+def guess(winner_gender):
+    if winner_gender == 'female':
+        fishers = female_fishers
+    else:
+        fishers = male_fishers
 
-    for country, probability in zip(countries, probabilities):
-        print("%s %.2f%%" % (country, probability)) # modify this to print correct results
+    # write your solution here
+    guess = None
+    biggest = 0.0
+    for i, population in enumerate(populations):
+        result = (fishers[i] / total_fishers) * 100
+        if result > biggest:
+            biggest = result
+            guess = countries[i]
     
-    return sum(probabilities) / len(probabilities)
+    return (guess, biggest)  
+
+def main():
+    country, fraction = guess("male")
+    print("if the winner is male, my guess is he's from %s; probability %.2f%%" % (country, fraction))
+    country, fraction = guess("female")
+    print("if the winner is female, my guess is she's from %s; probability %.2f%%" % (country, fraction))
 
 main()
