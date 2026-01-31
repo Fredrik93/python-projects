3c765003db857a7faa288b9c9c22ca860ce8613d
Fredrik93 <fredrikullman93@gmail.com>
2026-01-31 07:16:16 +0100

    test passing

--- elementsofai/cabin_prices.py
+++ elementsofai/cabin_prices.py
@@ -1,25 +1,18 @@
-# input values for three mökkis: 
-#  - size [m^2], 
-#  - size of the sauna [m^2], 
-#  - distance to water [m], 
-#  - number of indoor bathrooms, 
-#  - proximity of neighbors [m]
+# input values for three mökkis: size, size of sauna, distance to water, number of indoor bathrooms, 
+# proximity of neighbors
+import numpy as np
+
 X = [[66, 5, 15, 2, 500], 
      [21, 3, 50, 1, 100], 
      [120, 15, 5, 2, 1200]]
+Cl = [3000, 200, -50, 5000, 100]    # coefficient values
 
-# coefficient values
-c = [3000, 200 , -50, 5000, 100]
-price = 0            
+def predict(X, Cl):
+    price = 0
+    for data in X:     
+        x = np.array(data)
+        c = np.array(Cl)
+       
+        print(x @ c)
 
-def predict(X, c):
-    # write a loop that goes over the cabin data and for each cabin prints out 
-    for data in X:
-        price = c[0]*data[0] + c[1]*data[1] + c[2]*data[2] + c[3] * data[3] + c[4] * data[4]
-        print(price)
-
-    # the predicted price of the cabin you can assume that the number of inputs
-    # and the number of coefficients are the same
-    
-
-predict(X, c)
+predict(X, Cl)
