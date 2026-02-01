e95b7184f82b806abed35ec2092d5a29d59e7e15
Fredrik93 <fredrikullman93@gmail.com>
2026-02-01 13:15:33 +0100

    passing

--- elementsofai/cabin_prices_2.py
+++ elementsofai/cabin_prices_2.py
@@ -8,15 +8,19 @@ c = np.array([3000, 200 , -50, 5000, 100])    # coefficient values
  
 def squared_error(X, y, c):
     sse = 0.0
-    price_prediction = 0.0
+    sq =0.0
     for xi, yi in zip(X, y):
+        price_prediction = 0.0
+
         # add your code here: calculate the predicted price,
         for x, ci in zip (xi,c):
             price_prediction += x* ci
-        # subtract it from the actual price yi, 
+        # subtract it from the actual price yi,
+        error = yi - price_prediction
         # square the difference using (yi - prediction)**2, 
+        sq = (error)**2
         # and add up all the differences in variable sse
-        
+        sse += sq
 
     print(f"sse: {sse}")
 
