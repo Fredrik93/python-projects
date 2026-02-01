436079bd4fade8cbbf3af4b637ef5e2b5c6fabd5
Fredrik93 <fredrikullman93@gmail.com>
2026-02-01 07:40:46 +0100

    setup

--- elementsofai/cabin_prices.py
+++ elementsofai/cabin_prices.py
@@ -5,14 +5,12 @@ import numpy as np
 X = [[66, 5, 15, 2, 500], 
      [21, 3, 50, 1, 100], 
      [120, 15, 5, 2, 1200]]
-Cl = [3000, 200, -50, 5000, 100]    # coefficient values
+C = [3000, 200, -50, 5000, 100]    # coefficient values
 
-def predict(X, Cl):
-    price = 0
-    for data in X:     
-        x = np.array(data)
-        c = np.array(Cl)
-       
-        print(x @ c)
+def predict(X, C):
+    for mokki in X:
+        price = np.array(mokki) @ np.array(C)
+        print(price)
 
-predict(X, Cl)
+
+predict(X, C)
---
+++ tests/elementsofai/test_cabin_prices_2.py
@@ -0,0 +1,6 @@
+from elementsofai.cabin_prices_2 import squared_error
+
+def test():
+    def test_predict():
+       
+        assert(squared_error(1,1,1) == 1)
